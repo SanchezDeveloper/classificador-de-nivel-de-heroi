d0514ad78f197fc4066c2f055fd43558342e1e3a
Vinícius Caio <vinicsanchez@gmail.coom>
2025-11-21 17:59:57 -0300

    fix code

--- classificador.js
+++ classificador.js
@@ -1,23 +1,29 @@
 function classificadorDeNivel (name, xp) {
+  if(typeof xp !== "mumber" || Number.isNaN(xp)) {
+    throw new Error("XP tem que ser um numero e inteiro")
+  }
+  if(xp < 0) {
+    throw new Error("XP não pode ser negativo")
+  }
+
+
   let rank;
   if (xp < 1001) {
     rank = "Ferro";
-  } else if ( xp > 1000 && xp < 2001) {
+  } else if (xp < 2001) {
     rank = "Bronze";
-  } else if ( xp > 2000 && xp < 5001) {
+  } else if (xp < 5001) {
     rank = "Prata";
-  } else if ( xp > 5000 && xp < 7001) {
+  } else if (xp < 7001) {
     rank = "Ouro";
-  } else if ( xp > 7000 && xp < 8001) {
+  } else if (xp < 8001) {
     rank = "Platina";
-  } else if ( xp > 8000 && xp < 9001) {
+  } else if (xp < 9001) {
     rank = "Ascendente";
-  } else if ( xp > 9000 && xp < 10001) {
+  } else if (xp < 10001) {
     rank = "Imortal";
   } else if ( xp >= 10001) {
     rank = "Radiante";
-  } else {
-    console.erro("não existe xp menor que zero");
   }
 
   console.log(`O Herói de nome: ${name} está no nível ${rank}`)
